0f4d9da5986302b96d711222f37270092b52c711
Akash A <akasha14122001@gmail.com>
2023-11-01 23:01:50 +0530

    Commit Changes

--- RetailCaseStudy.py
+++ RetailCaseStudy.py
@@ -18,12 +18,12 @@ Cust_Final.to_csv(a, index=False)'''
 
 # 2. Summary Report
 
-#     Column names with their data-types
+#     a. Column names with their data-types
 print(Cust_Final.dtypes)
-#     Top/Bottom 10 Observations
+#     b. Top/Bottom 10 Observations
 print(Cust_Final.head(10))
 print(Cust_Final.tail(10))
-#     Five-number summary for continuous variables (min, Q1, median, Q3 and max)
+#     c. Five-number summary for continuous variables (min, Q1, median, Q3 and max)
 Data_min = Cust_Final['total_amt'].min()
 Data_max = Cust_Final['total_amt'].max()
 Data_q1  = np.percentile(Cust_Final.total_amt,25)
@@ -34,10 +34,41 @@ print('Maximum = ',Data_max)
 print('Median = ',median)
 print('Q1 = ',Data_q1)
 print('Q3 = ',Data_q3)
+#     d. Frequency tables for all the categorical variables
+freq_table1 = pd.crosstab(index = Cust_Final['Gender'],columns = Cust_Final['Store_type'])
+freq_table1.columns = ['TeleShop','MBR','e-shop','Flagshipstore']
+freq_table1.index = ['Male','Female']
+print(freq_table1)
+
+freq_table2 = pd.crosstab(index = Cust_Final['Gender'],columns = Cust_Final['prod_cat'])
+freq_table2.columns = ['Books','Bags','Clothing','Footwear','Electronics','Home and kitchen']
+freq_table2.index = ['Male','Female']
+print(freq_table2)
+
+freq_table3 = pd.crosstab(index = Cust_Final['Gender'],columns = Cust_Final['prod_subcat'])
+freq_table3.columns = ['Men','Women','Kid','Mobile','Computer','Personal Appliances','Cameras','Audio and video',
+                      'Fiction','Academic','Non-fiction','Children','Comics','DIY','Furnishing','Kitchen',
+                      'Bath','Tools']
+freq_table3.index = ['Male','Female']
+print(freq_table3)
+
 
 # 3. Generate histograms for all continuous variables and frequency bars for categorical variables
 
    # Histogram
+Tax = Cust_Final['Tax']
+plt.hist(Tax,color=['blue'])
+plt.xlabel('tax')
+plt.ylabel('Frequency')
+plt.title("3a")
+plt.show()
+
+Total_Amt = Cust_Final['total_amt']
+plt.hist(Total_Amt,color = 'green')
+plt.xlabel('Total amount')
+plt.ylabel('Frequency')
+plt.title("3a")
+plt.show()
 
    # Frequency Bars
           # For Store Type
@@ -46,6 +77,7 @@ plt.bar(Val1['Store_type'],Val1['total_amt'])
 plt.xlabel('Store Type')
 plt.ylabel('Total Amount')
 plt.title('Total Amount by Store Type')
+plt.title("3b")
 plt.show()
 
           # For Sub Category
@@ -55,6 +87,7 @@ plt.xlabel('Product Sub Category')
 plt.ylabel('Total Amount')
 plt.title('Total Amount by Product Sub Category')
 plt.xticks(rotation=45)
+plt.title("3b")
 plt.show()
 
           # For Gender
@@ -63,11 +96,26 @@ plt.bar(Val3['Gender'],Val3['Qty'])
 plt.xlabel('Quantity')
 plt.ylabel('Total Amount')
 plt.title('Total Amount by Product Sub Category')
+plt.title("3b")
 plt.show()
 
 # 4. Using Merged Dataset
 
    # a. Time period of the available transaction data
+years = Cust_Final['tran_date'].str.extract(r'(\d{4})')
+y1 = years.value_counts().get('2011', 0)
+y2 = years.value_counts().get('2012', 0)
+y3 = years.value_counts().get('2013', 0)
+y4 = years.value_counts().get('2014', 0)
+print(y1)
+print(y2)
+print(y3)
+print(y4)
+values=[y1,y2,y3,y4]
+piechart=plt.pie(values)
+plt.title("4a")
+plt.show()
+
 
    # b. Count of transactions where the total amount of transaction was negative
 df = Cust_Final['total_amt']
@@ -123,7 +171,20 @@ df1 = dl[(dl.total_amt > 0)]
 ts = df1.transaction_id.nunique()
 print('Total customers having more than 10 unique transactions are : ',ts)
 
-print("sucess")
-print("fkysegfe")
+# 11. For all customers aged between 25 - 35, find out
 
+dn = pd.DataFrame(Cust_Final)
+curr_year = pd.to_datetime('today').year
+dob_year = pd.DatetimeIndex(dn['DOB']).year
+x = dob_year-100
+v6 = curr_year - x
+y6 = curr_year - dob_year
+dn['age'] = (np.where(dob_year > curr_year,v6,y6))
 
+        #  a. What was the total amount spent for “Electronics” and “Books” product categories
+total = dn.loc[((dn.age >25) & (dn.age <35)) & ((dn.prod_cat=='Books') | (dn.prod_cat=='Electronics'))]['total_amt'].sum()
+print('Total amount earned',total)
+        #  b. What was the total amount spent by these customers between 1st Jan, 2014 to 1st Mar, 2014?
+t_date = Cust_Final[(Cust_Final['tran_date'] > '01-01-2014') & (Cust_Final['tran_date'] < '01-03-2014')]
+total_amount = t_date.total_amt.sum()
+print('Total amount spent by the customer - ',total_amount)
